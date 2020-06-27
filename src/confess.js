3e7823a1a268514d4d88bae3cf21a1d30011069b
Gabriel Ting <gabrielfting@gmail.com>
2020-06-27 16:08:29 -0500

    Add confessions slash command

---
+++ src/confess.js
@@ -0,0 +1,46 @@
+const { text } = require("micro");
+const { parse } = require("querystring");
+const fetch = require("node-fetch");
+
+module.exports = async (req, res) => {
+	// get the text that the user submitted with the feedback
+	let parsedText, response_url;
+	try {
+		// gets the application/x-www-form-urlencoded string
+		const body = parse(await text(req));
+		parsedText = body.text;
+		response_url = body.response_url;
+	} catch (e) {
+		res.status(400).send({
+			text: "There was an error while parsing the request.",
+		});
+	}
+
+	if (parsedText) {
+		// sends back the feedback quoted
+		const resObject = {
+			text: `There's a new confession!\n>${parsedText}`,
+			response_type: "in_channel",
+		};
+
+		// send the feedback back to the channel via post
+		fetch(process.env.SLACK_WEBHOOK_CONFESS, {
+			method: "post",
+			body: JSON.stringify(resObject),
+			headers: { "Content-Type": "application/json" },
+		}).then((feedbackRes) => {
+			if (feedbackRes.ok) {
+				res.status(200).send({
+					text:
+						"Successfully sent your feedback to the #anon-confessions channel!",
+				});
+			}
+			res.status(400).send();
+		});
+		// res.status(200).send(resObject);
+	} else {
+		res.status(404).send({
+			error: "There was either no text or there was no response url.",
+		});
+	}
+};
